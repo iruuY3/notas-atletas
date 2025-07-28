6ea20d490a624fc51f32bbe2c4131a3fd4ce98e0
Yuri Fernando <yurigamer1003@gmail.com>
2025-07-28 15:40:50 -0300

    Create notas-atletas.js

---
+++ notas-atletas.js
@@ -0,0 +1,33 @@
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+let notas = 0
+for (let i = 0; i < atletas.length ; i++){
+  let soma = 0
+  notas = atletas[i].notas.sort((a,b) => a - b)
+  let notasComputadas = notas.slice(1,4)
+  notasComputadas.forEach(function(n){
+    soma = soma + n
+  })
+  notas = notas.join(" ,")
+  soma = soma/notasComputadas.length
+  console.log(`Atleta: ${atletas[i].nome}`)
+  console.log(`Notas Obtidas: ${notas}`)
+  console.log(`Média Válida: ${soma}`)
+  console.log(`-----------------------`)
+}
